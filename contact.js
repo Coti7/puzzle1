94705c0cb33f5034ded37ed33bb341aac738f8b4
Coti <108825505+Coti7@users.noreply.github.com>
2025-04-14 21:18:27 +0200

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("submitBtn").addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("Bruh ahh firefox");
+
+    alert("bombartididlo teorotii ttreeetiit!?!?!??!")
+    document.getElementById("captchaInput").value = "";
+  });
+});
